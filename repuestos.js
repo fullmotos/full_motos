143d2c1ec1319d24cc3e05b8c0b4a220fc272f3d
fullmotos <elparche1021@gmail.com>
2026-01-05 14:51:10 -0500

    Update repuestos.js

--- repuestos.js
+++ repuestos.js
@@ -15,31 +15,27 @@ function filtrarCategoria(categoria) {
     });
 }
 
+------------------------------------------------------------------------------------------------------
 
-function cambiarPrecio(select) {
-  const precio = select.value;
-  const card = select.closest('.producto-card');
-  const precioSpan = card.querySelector('.precio-actual');
 
-  precioSpan.textContent = `$${Number(precio).toLocaleString()}`;
-}
-
-function agregarAlCarrito(nombre, boton) {
-  const card = boton.closest('.producto-card');
-  const select = card.querySelector('.marca-select');
-  const precio: { valor: 35000, marca: "CHOHO" };
-  const marca = select.options[select.selectedIndex].dataset.marca;
-
+    function agregarAlCarrito(nombre, marca, precio) {
   let carrito = JSON.parse(localStorage.getItem('carrito')) || [];
 
-  carrito.push({
-    nombre,
-    marca,
-    precio: Number(precio),
-    cantidad: 1
-  });
+  const existente = carrito.find(
+    item => item.nombre === nombre && item.marca === marca
+  );
+
+  if (existente) {
+    existente.cantidad += 1;
+  } else {
+    carrito.push({
+      nombre,
+      marca,
+      precio,
+      cantidad: 1
+    });
+  }
 
   localStorage.setItem('carrito', JSON.stringify(carrito));
-
-  alert(`Agregado al carrito:\n${nombre}\nMarca: ${marca}`);
+  actualizarContadorCarrito();
 }
